a038dffcb08a7e90f4e3980bbcb39e88271add51
bstunney <108998767+bstunney@users.noreply.github.com>
2023-01-12 19:15:55 -0500

    Update main.py

--- main.py
+++ main.py
@@ -605,6 +605,7 @@ def main():
     external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
     app = Dash(__name__, external_stylesheets=external_stylesheets)
+    server = app.server
     app.layout = html.Div(id='App', children=[
 
         # top line of Dash
